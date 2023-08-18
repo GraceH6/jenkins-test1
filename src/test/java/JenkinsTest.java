49800365d8f48ea15e9ff43344c049bb21523c2f
Zhansaya Kalsat <zhansayakalsat@gmail.com>
2023-08-18 16:21:45 +0600

    initial

---
+++ src/test/java/JenkinsTest.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class JenkinsTest {
+    WebDriver driver;
+    @Test
+    void loginAndLogoutTest() {
+        String expTitle = "Sample App";
+        String userName = "Grace";
+        String password = "pwd";
+        String expLoginMessage = "Welcome, " + userName + "!";
+        String expLogoutMessage = "User logged out.";
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://uitestingplayground.com/sampleapp");
+        String title = driver.getTitle();
+        assertEquals(title, expTitle, "Sample App page is displayed");
+
+        WebElement userNameInput = driver.findElement(By.xpath("//input[@name='UserName']"));
+        userNameInput.sendKeys(userName, Keys.ENTER);
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@name='Password']"));
+        passwordInput.sendKeys(password, Keys.ENTER);
+        WebElement logButton = driver.findElement(By.xpath("//button[@id='login']"));
+        logButton.click();
+        WebElement status = driver.findElement(By.xpath("//label[@id='loginstatus']"));
+        String loginMessage = status.getText();
+        assertEquals(loginMessage, expLoginMessage, "Welcome page is displayed");
+
+        logButton.click();
+        String logoutMessage = status.getText();
+        assertEquals(logoutMessage, expLogoutMessage, "User logged out message is displayed");
+
+        driver.quit();
+    }
+}
